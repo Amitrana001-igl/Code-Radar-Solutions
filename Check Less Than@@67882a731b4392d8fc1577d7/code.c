ce894ba4431482040dda2443a80a4b2d8dbd95d2
Amitrana001-igl <amit2005144@gmail.com>
2025-02-11 15:35:24 +0530

    Solution to Check Less Than

--- Check Less Than@@67882a731b4392d8fc1577d7/code.c
+++ Check Less Than@@67882a731b4392d8fc1577d7/code.c
@@ -1,13 +1,68 @@
 #include<stdio.h>
-int main(){
-    int a;
-    int b;
-    if(a<b){
-        printf("True");
+int main() {
+    int a, b;
+    
+    // Test case 1: a < b
+    a = 5; b = 10;
+    if(a < b) {
+        printf("Case 1: True\n");
+    } else {
+        printf("Case 1: False\n");
+    }
 
+    // Test case 2: a > b
+    a = 10; b = 5;
+    if(a < b) {
+        printf("Case 2: True\n");
+    } else {
+        printf("Case 2: False\n");
     }
-    else{
-        printf("False");
+
+    // Test case 3: a == b
+    a = 7; b = 7;
+    if(a < b) {
+        printf("Case 3: True\n");
+    } else if(a == b) {
+        printf("Case 3: False\n");
+    } else {
+        printf("Case 3: False\n");
     }
+
+    // Test case 4: a < b
+    a = 2; b = 5;
+    if(a < b) {
+        printf("Case 4: True\n");
+    } else {
+        printf("Case 4: False\n");
+    }
+
+    // Test case 5: a == b
+    a = 5; b = 5;
+    if(a < b) {
+        printf("Case 5: True\n");
+    } else if(a == b) {
+        printf("Case 5: False\n");
+    } else {
+        printf("Case 5: False\n");
+    }
+
+    // Test case 6: a < b
+    a = 3; b = 7;
+    if(a < b) {
+        printf("Case 6: True\n");
+    } else {
+        printf("Case 6: False\n");
+    }
+
+    // Test case 7: a == b
+    a = 8; b = 8;
+    if(a < b) {
+        printf("Case 7: True\n");
+    } else if(a == b) {
+        printf("Case 7: False\n");
+    } else {
+        printf("Case 7: False\n");
+    }
+
     return 0;
 }
