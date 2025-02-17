a70a9fc3ff147bec8fadc1a4ffbcc0ac6e9232ac
Amit rana <amit2005144@gmail.com>
2025-02-18 01:35:09 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,24 +1,22 @@
+
+
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);  // Input two integers
-
-    // Check different operations and print the result
-    if (a + b != 0) {
-        printf("%d", a + b);  // Print the sum of a and b if it's non-zero
-    }
-    else if (a - b != 0) {
-        printf("%d", a - b);  // Print the difference if it's non-zero
-    }
-    else if (a * b != 0) {
-        printf("%d", a * b);  // Print the product if it's non-zero
-    }
-    else if (b != 0) {
-        printf("%d", a / b);  // Print the division result if b is non-zero
-    }
-    else {
-        printf("Invalid operation");
+    float num1, num2;
+    char operator;
+    
+    scanf("%f %f %c", &num1, &num2, &operator);
+    
+    switch (operator) {
+        case '+': printf("%.2f\n", num1 + num2); break;
+        case '-': printf("%.2f\n", num1 - num2); break;
+        case '*': printf("%.2f\n", num1 * num2); break;
+        case '/': 
+            if (num2 != 0) printf("%.2f\n", num1 / num2);
+            else printf("Error! Division by zero.\n");
+            break;
+        default: printf("Invalid operator!\n");
     }
 
     return 0;
